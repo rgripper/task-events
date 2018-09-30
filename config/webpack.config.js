e4fa038d7d85e8d3658be32b7cbb32bbfa3e29ef
Vladimir Zotov <azuresplash@gmail.com>
2018-09-30 15:50:40 +1000

    Added external settings to the build config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,26 +1,31 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const paths = require('./paths');
+const appSettings = require('../appSettings.json');
+
+const isDevServer = process.argv.some(v => v.includes('webpack-dev-server'));
 
 module.exports = {
   entry: paths.appIndex,
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-eval-source-map',
+  externals: {
+    appSettings
+  },
   module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-    plugins: [new TsconfigPathsPlugin({ })]
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin({})]
   },
   output: {
-    filename: 'bundle.js',
-    path: paths.appBuild
+    filename: isDevServer ? '[name].[hash].js' : '[name].[chunkhash].js',
+    path: paths.appBuild,
+    publicPath: '', // relative path for Github deployment
   },
   plugins: [
     new HtmlWebpackPlugin({
